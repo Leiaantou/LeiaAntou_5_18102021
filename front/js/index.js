5af79117fac8a37080a489a98617f694701232c4
Leia Antou <antou.leia@gmail.com>
2021-11-07 23:20:53 +0100

    Ajout des produits sur la page d'accueil

--- front/js/index.js
+++ front/js/index.js
@@ -1,35 +1,39 @@
-//   const products = getProducts();
+// const products = getProducts();
 // console.log(products);
 displayProducts();
 async function getProducts() {
-
-    return await fetch("http://localhost:3000/api/products")
-        .then(function (res) {
-            // if (res.ok) {
-            //  console.log (res.json())
-            return res.json();
-            // }
-        })
-        .then(function (value) {
-            console.log(value);
-            return value;
-        })
-        .catch(function (err) {
-            // Une erreur est survenue
-            console.log(err);
-        });
+  return await fetch("http://localhost:3000/api/products")
+    .then(function (res) {
+      // if (res.ok) {
+      //  console.log (res.json())
+      return res.json();
+      // }
+    })
+    .then(function (value) {
+      console.log(value);
+      return value;
+    })
+    .catch(function (err) {
+      // Une erreur est survenue
+      console.log(err);
+    });
 }
+
 async function displayProducts() {
-    const products = await getProducts();
-    console.log("displayProducts", products);
-    let productsItems = '';
-    productsItems += '          <a href="./product.html?id=' + products.Id + '">' +
-        '<article>' +
-        '<img src=".../product01.jpg" alt="Lorem ipsum dolor sit amet, Kanap name1">' +
-        '<h3 class="productName">Kanap name1</h3>' +
-        '<p class="productDescription">Dis enim malesuada risus sapien gravida nulla nisl arcu. Dis enim malesuada risus sapien gravida nulla nisl arcu.</p>' +
-        '</article>' +
-        '</a>'
+  const products = await getProducts();
+  console.log("displayProducts", products);
+  let productsSection = document.getElementById("items");
+  for (i = 0; i < products.length; i++) {
+    let productsItems = `
+      <a href="./product.html?id=${products[i]._id}">
+      <article>
+      <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+      <h3 class="productName">${products[i].name}</h3>
+      <p class="productDescription">${products[i].description}</p>
+      </article> 
+      </a>`;
+    productsSection.innerHTML += productsItems;
+  }
 }
 // manipuler le tableau de produits en bouclant grâce à JS pour parcourir tous les elements
 // créer une variable productsItems qui contient une chaine de caracteres vide
